17d0f6ce0cfd549bd943ab927a2118d743bf66a6
Parthipan-Natkunam <natkunamparthipan@gmail.com>
2019-11-09 23:48:29 +0530

    initial commit

---
+++ src/containers/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import  style from '../style/App.module.css';
+
+function App() {
+  return (
+    <div className={style.App}>
+      
+    </div>
+  );
+}
+
+export default App;
