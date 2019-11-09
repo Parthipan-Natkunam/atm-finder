39f73c234c56a2fd277e7b7318e9dc9843636a2f
Parthipan-Natkunam <natkunamparthipan@gmail.com>
2019-11-10 02:05:26 +0530

    refactor getCurrentLocation promise

--- src/utils/utility.js
+++ src/utils/utility.js
@@ -1,15 +1,22 @@
 export const getCurrentLocation = ()=>{
+    
     return new Promise((resolve,reject)=>{
+        
         if (!navigator.geolocation) {
             reject("Geolocation not supported by the browser");
-           } else {
-             navigator.geolocation.getCurrentPosition((position)=>{
-                const {latitude:lat,longitude:lng}   = position.coords;
-                if(typeof lat === void 0 || typeof lng === void 0){
-                    reject("Browser returned invalid location");
-                }
-                resolve({lat,lng});
-             }, ()=>reject("Unable to get device location"));
-           }
+        } else {
+            navigator.geolocation.getCurrentPosition(
+                //success callback
+                (position)=>{
+                    const {latitude:lat,longitude:lng}   = position.coords;
+                    if(typeof lat === void 0 || typeof lng === void 0){
+                        reject("Browser returned invalid location");
+                    }
+                    resolve({lat,lng});
+                }, 
+                 //error callback
+                ()=>reject("Unable to get device location")
+            );
+        }
     });
 }
