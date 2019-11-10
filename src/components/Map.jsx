9feb4963da2ac7c50682d1d001ab64f9d47cbd93
Parthipan-Natkunam <natkunamparthipan@gmail.com>
2019-11-10 20:49:11 +0530

    refactor map component to utilize state

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -2,15 +2,16 @@ import React from "react";
 import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
 
 const Map = withScriptjs(withGoogleMap((props) =>{
+    const {isMarkerShown, userLocation} = props;
+    const {lat, lng} = userLocation;
     return (
         <GoogleMap
             defaultZoom={8}
-            defaultCenter={{ lat: -34.397, lng: 150.644 }}
+            defaultCenter={{ lat: 12.9716, lng: 77.5946 }} //show default map center as Bengaluru
         >
-            {props.isMarkerShown && <Marker position={{ lat: -34.397, lng: 150.644 }} />}
+            {isMarkerShown && userLocation && <Marker position={{ lat, lng }} />}
         </GoogleMap>
     );
 }));
-
 
 export default Map;
--- src/containers/MapContainer.jsx
+++ src/containers/MapContainer.jsx
@@ -3,10 +3,17 @@ import React from 'react';
 import GoogleMap from '../components/Map';
 
 class MapContainer extends React.Component{
+    state={
+        isMarkerShown: false,
+        userLocation: null
+    }
+
     render(){
         const mapSdkUri = `https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_MAP_API_KEY}&libraries=places`;
+        const {isMarkerShown, userLocation} = this.state;
         return(
-            <GoogleMap isMarkerShown
+            <GoogleMap isMarkerShown={isMarkerShown}
+                userLocation = {userLocation}
                 googleMapURL={mapSdkUri}
                 loadingElement={<div style={{ height: `100%` }} />}
                 containerElement={<div style={{ height: `400px` }} />}
